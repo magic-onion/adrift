1925e1d160bbc90ad1749b67768f73cafecd49c2
magic-onion <alexander.m.truong@gmail.com>
2019-02-04 08:40:57 -0500

    Updates

---
+++ precache-manifest.d1631ba89361baa2fb06c4a10f8364d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb39656d6b50050ddcc5",
+    "url": "/adrift/static/css/main.f9c07f75.chunk.css"
+  },
+  {
+    "revision": "eb39656d6b50050ddcc5",
+    "url": "/adrift/static/js/main.eb39656d.chunk.js"
+  },
+  {
+    "revision": "e29b121a5e8fc5ecb90b",
+    "url": "/adrift/static/js/1.e29b121a.chunk.js"
+  },
+  {
+    "revision": "64e2674b866e6c5d234a",
+    "url": "/adrift/static/js/runtime~main.64e2674b.js"
+  },
+  {
+    "revision": "3b53dcdd4212fb4a81d11081218c46c8",
+    "url": "/adrift/static/media/dog-bark.3b53dcdd.mp3"
+  },
+  {
+    "revision": "2246eeed378725c68cc33a1da6329682",
+    "url": "/adrift/static/media/losing-mind.2246eeed.mp3"
+  },
+  {
+    "revision": "08af26fb93ca54b0b838d8e443d87f99",
+    "url": "/adrift/static/media/monster-growl.08af26fb.mp3"
+  },
+  {
+    "revision": "753ea6cbad970a124b09bfc782c74594",
+    "url": "/adrift/static/media/you-died.753ea6cb.mp3"
+  },
+  {
+    "revision": "2a168d24431a555b68fca940ef545e5f",
+    "url": "/adrift/static/media/win.2a168d24.mp3"
+  },
+  {
+    "revision": "92f702c2d5df2293267dac5173537cd2",
+    "url": "/adrift/index.html"
+  }
+];
